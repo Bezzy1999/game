832fb2d047e648ec7b3652b91074bd42501917ee
Bezzy1999 <bezzy1999@users.noreply.github.com>
2018-02-02 21:54:56 +0000

    Initial setup of level

--- src/levels.py
+++ src/levels.py
@@ -1,8 +1,9 @@
+import pygame
 
 level1 = [
 "BBBBBBBBBBBBBBBBBBBB",
-"BBBBBBBBBBBBBBBBBBBB",
-"BBBBBBBBBBBBBBBBBBBB",
+"B B   BBBBBBBBBBBBBB",
+"BBB   BBBBBBBBBBBBBB",
 "BBBBBBBBBBBBBBBBBBBB",
 "BBBBBBBBBBBBBBBBBBBB",
 "BBBBBBBBBBBBBBBBBBBB",
@@ -21,3 +22,18 @@ level1 = [
 levels = [
     level1
 ]
+
+level_image = pygame.image.load('art/element_blue_square.png')
+
+def createLevel(level):
+    rects = []
+    for row_index, row in enumerate(level):
+        for col_index, col in enumerate(row):
+            if col == 'B':
+                rects.append(pygame.Rect(col_index * 32, row_index * 32, 32, 32))
+
+    return rects
+
+def drawLevel(rects, screen):
+    for rect in rects:
+        screen.blit(level_image, rect)
