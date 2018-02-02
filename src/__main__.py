393d2d0b0b03999f2a5c72b78a992bf50761e9ae
Bezzy1999 <bezzy1999@users.noreply.github.com>
2018-02-02 19:41:44 +0000

    Added basic window

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,15 @@
+import sys
 import pygame
 
 
 def main():
-	pygame.init()
+    pygame.init()
 
+    screen = pygame.display.set_mode((640, 480))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
 
 if __name__ == '__main__':
-	main()
+    main()
