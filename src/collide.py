d52fd7cddce6506ee3f131e2ba04e73643491804
Bezzy1999 <bezzy1999@users.noreply.github.com>
2018-02-24 03:34:24 +0000

    Some basic collision

--- src/collide.py
+++ src/collide.py
@@ -1,3 +1,5 @@
+import math 
+
 
 def check(ball, level, paddle):
     ballRect = ball.rect
@@ -18,6 +20,32 @@ def collides(rect1, rect2):
     if (rect1.y + rect1.height) < rect2.y: return False
 
     return True
+
+def dist_between_points(x1, y1, x2, y2):
+    dist = x1 - x2
+    dist2 = y1 - y2
+    dist *= dist
+    dist2 *= dist2
+
+    return dist + dist2
+
+def collideBallVsRect(ballPos, radius, rect):
+    rect_centreX = rect.x + (rect.width / 2)
+    rect_centreY = rect.y + (rect.height / 2)
+
+    distx = math.fabs(ballPos.x - rect_centreX)
+    disty = math.fabs(ballPos.y - rect_centreY)
+
+    if distx > ((rect.width / 2) + radius): return False
+    if disty > ((rect.height / 2) + radius): return False
+
+    if distx <= (rect.width / 2): return True
+    if disty <= (rect.height / 2): return True
+
+    dist_sq = math.pow(distx - (rect.width / 2), 2) + \
+                math.pow(disty - (rect.height / 2), 2)
+
+    return dist_sq <= (radius * radius)
 
 def respond_to_collision(ball, rect):
     ball.rect.x = ball.rect.x - ball.speed[0]
--- tests/test_collide.py
+++ tests/test_collide.py
@@ -7,6 +7,11 @@ class rect(object):
         self.width = width
         self.height = height
 
+class rect_with_speed(rect):
+    def __init__(self, pos, speed):
+        self.rect = rect(pos.x, pos.y, pos.width, pos.height)
+        self.speed = speed
+
 def test_collides_rect1_leftof_rect2_not_touching():
     rect1 = rect(2, 2, 4, 4)
     rect2 = rect(7, 2, 4, 4)
@@ -54,3 +59,19 @@ def test_collides_rect1_below_rect2_touching():
     rect2 = rect(3, 7, 7, 4)
 
     assert collide.collides(rect1, rect2)
+
+def test_respond_to_collision_bounce_above_left_to_right():
+    rect1 = rect(2, 2, 4, 4)
+    ball = rect_with_speed(rect1, [2, 2])
+    rect2 = rect(2, 5, 6, 4)
+
+    collide.respond_to_collision(ball, rect2)
+    assert ball.speed == [2, -2]
+
+def test_respond_to_collision_bounce_below_left_to_right():
+    rect1 = rect(2, 8, 4, 4)
+    ball = rect_with_speed(rect1, [2, -2])
+    rect2 = rect(2, 2, 6, 2)
+
+    collide.respond_to_collision(ball, rect2)
+    assert ball.speed == [2, 2]
